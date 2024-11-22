a82328d370725f2a25be8176eb23bae4c6f92cf7
Connor Auyong <connorauyong@gmail.com>
2024-11-22 12:09:52 -0800

    Initial Commit

---
+++ PDFListen/PDFListenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFListenApp.swift
+//  PDFListen
+//
+//  Created by Connor Auyong on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct PDFListenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PDFListenTests/PDFListenTests.swift
@@ -0,0 +1,16 @@
+//
+//  PDFListenTests.swift
+//  PDFListenTests
+//
+//  Created by Connor Auyong on 11/22/24.
+//
+
+import Testing
+
+struct PDFListenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
